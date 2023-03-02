6aab1d79939ba23113a7f64df2befb2875ec4a37
Mark Harfouche <mark.harfouche@gmail.com>
2023-03-02 07:40:39 -0500

    manually disable SSE

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,23 @@
 # Get an updated config.sub and config.guess
+set -ex
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 mkdir build
 cd build
+
+# Manually disable SSE for platforms that don't support it
+# in hardware 
+# https://github.com/strukturag/libde265/issues/308
+EXTRA_FLAGS=
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  EXTRA_FLAGS="${EXTRA_FLAGS} -DDISABLE_SSE=ON"
+fi
 
 cmake ${CMAKE_ARGS} -G "Ninja" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCMAKE_PREFIX_PATH="$PREFIX" \
   -DCMAKE_SYSTEM_PREFIX_PATH="$PREFIX" \
   -DCMAKE_BUILD_TYPE="Release" \
+  ${EXTRA_FLAGS} \
   ..
 
 ninja
